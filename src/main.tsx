0bac16481742d56557a93b3b8fcc830443ef3efb
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2023-07-10 23:11:46 +0200

    form collects name, an egg num, and funmode toggle
    todo: generate genotype, create post, populate kv store

--- src/main.tsx
+++ src/main.tsx
@@ -24,28 +24,60 @@ Devvit.addSchedulerJob({
 const formKeyNewSnoomagotchi = Devvit.createForm({
   title: "Create a new Snoomagotchi",
   description: "Use this form to create a brand new snoomagotchi woohoo",
-  acceptLabel: "Hatch",
-  cancelLabel: "Abandon",
+  acceptLabel: "Hatch!",
+  cancelLabel: "Cancel",
   fields: [
     {
       type: "string",
       name: "virtualPetName",
-      label: "Snoomagotchi's Name",
+      label: "What Will Your Snoomagotchi's Name Be?",
+      helpText: "Snoomagotchi are genderless. We reserve the right to destroy Snoomagotchi that have inappropriate or offensive names.",
       required: true,
+    },
+    {
+      type: "select",
+      name: "eggNumber",
+      label: "Pick Your Egg",
+      helpText: "The system has generated three unique genotypes for you to select from.",
+      required: true,
+      options: [
+        {
+          label: "Egg #1",
+          value: "1",
+        },
+        {
+          label: "Egg #2",
+          value: "2",
+        },
+        {
+          label: "Egg #3",
+          value: "3",
+        }
+      ]
+    },
+    {
+      type: "boolean",
+      name: "funMode",
+      label: "Enable Fun Mode",
+      helpText: "Fun-Mode Snoomagotchi cannot die, cannot breed, and do not contribute to population metrics. You may have up to three fun-mode Snoomagotchi."
     }
   ]
 },
 (event, context) => {
   const virtualPetName = event.values["virtualPetName"] as string;
+  const eggNumber = event.values["eggNumber"] as number;
+  const funMode = event.values["funMode"] as boolean;
 
-  context.ui.showToast(`the new virtual pet is called ${virtualPetName}`);
+  // todo generate genotype, virtualpet structure, make the post, populate kv store
+
+  context.ui.showToast(`the new virtual pet is called ${virtualPetName}, it has egg ${eggNumber} and funMode=${funMode}`);
 });
 
 Devvit.addMenuItem({
   label: "Create My Snoomagotchi!",
   description: "Create a new Snoomagotchi for the currently logged in user.",
   location: "subreddit",
-  forUserType: ["member", "moderator"],
+  forUserType: ["member"],
   onPress: (_, context) => context.ui.showForm(formKeyNewSnoomagotchi)
 });
 
