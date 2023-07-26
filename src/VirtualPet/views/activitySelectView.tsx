b081da31e9e2a9563180ec27019646841c6d6231
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2023-07-26 20:06:56 +0200

    remove weird files

--- src/VirtualPet/views/activitySelectView.tsx
+++
@@ -1,8 +0,0 @@
-import { Devvit } from "@devvit/public-api";
-import { VirtualPetComponent } from "../types/VirtualPetComponent.js";
-
-const ActivitySelectView: VirtualPetComponent = (context) => {
-  return (<text>activitySelectView</text>);
-};
-
-export default ActivitySelectView;
--- src/VirtualPet/views/finishedActivityView.tsx
+++
@@ -1,8 +0,0 @@
-import { Devvit } from "@devvit/public-api";
-import { VirtualPetComponent } from "../types/VirtualPetComponent.js";
-
-const FinishedActivityView: VirtualPetComponent = (context) => {
-  return (<text>finishedActivityView</text>);
-};
-
-export default FinishedActivityView;
