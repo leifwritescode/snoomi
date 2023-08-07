6fefa2a88540700b12e2bec12a89f280548c4597
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2023-08-07 06:10:59 +0200

    Apply tick fixes to age

--- src/VirtualPet/jobs/ageTickJob.ts
+++ src/VirtualPet/jobs/ageTickJob.ts
@@ -1,11 +1,25 @@
 import { ScheduledJobHandler } from "@devvit/public-api";
 import { VirtualPet } from "../VirtualPet.js";
+import { REDIS_KEY_AGE_TICK_BATCHES } from "../constants.js";
 
 const ageTickJob: ScheduledJobHandler = async (_, { kvStore }) => {
-  console.log("age tickover");
-  const keys = await kvStore.list();
-  for (const key of keys) {
-    let value = await kvStore.get<string>(key);
+  const batches = await kvStore.get<string[][]>(REDIS_KEY_AGE_TICK_BATCHES);
+  if (batches === undefined) {
+    console.log("No age batches are configured.");
+    return;
+  }
+
+  const now = new Date();
+  console.log(`Begin processing age tick batch D+${now.getHours()} (Time is ${now}))`);
+
+  const pets = batches[now.getHours()];
+  if (pets.length === 0) {
+    console.log("No pets to process.");
+    return;
+  }
+
+  for (const pet of pets) {
+    let value = await kvStore.get<string>(pet);
     if (value === undefined) {
       continue;
     }
@@ -14,8 +28,10 @@ const ageTickJob: ScheduledJobHandler = async (_, { kvStore }) => {
     virtualPet.age++;
 
     value = JSON.stringify(virtualPet);
-    await kvStore.put(key, value);
+    await kvStore.put(pet, value);
   }
+
+  console.log(`Finished processing ${pets.length} pets in batch D+${now.getHours()}.`);
 }
 
 export default ageTickJob;
