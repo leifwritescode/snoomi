28bec459495017d165a86556060b452dcb0e954c
Leif Walker-Grant <24778021+LeifWritesCode@users.noreply.github.com>
2023-08-11 00:50:04 +0200

    thoughts, posterity.

--- src/VirtualPet/types/Genotype.ts
+++ src/VirtualPet/types/Genotype.ts
@@ -16,7 +16,20 @@ export type Genotype = {
     hunger: Gene;
 
     // Hidden: Influences measures that aren't explicitly exposed to the player.
+    // proposal: we replace weight with a set of "food preferences" genes that influence the pets nutritional needs.
+    // failing to meet a pets nutritional needs may result in sickness.
+    // sickness is so far has a pretty identifiable cause: hunger, unhappiness, and unsanitary environment.
+    // i'm not sure how we model this in a sensiible way yet, but my brain juices are going
     weight: Gene;
+
+    // similar story here. having this as a -/+ scale is a bit opinionated and some might find it offensive
+    // i think that we can make this one work, though.
+    // intelligence is a positive scale that only impacts how the pet feels about cerebral games.
+    // we introduce another gene, sportiness that impacts how the pet feels about physical games.
+    // perhaps a third gene, for somewhere in between.
     intelligence: Gene;
+
+    // not sure if we actually need this one. the idea of the aging gene is to influence how rapidly the pet can evolve
+    // but there are already a lot of genes that influence the rate of change in the pet.
     aging: Gene;
 }
